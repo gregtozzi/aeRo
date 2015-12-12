93eedeccf5a22c6484a5ecf585b92e05c3d7fd9b
Greg Tozzi <gtozzi@alum.mit.edu>
2015-12-11 21:35:28 -0500

    Committing significant rewrite of the Rcpp functions and the helper functions.  The lumped vortex method is largely complete with this commit.

--- helper_functions.R
+++ helper_functions.R
@@ -1,8 +1,60 @@
 require(MASS)
+require(pracma)
 
 foilPlot <- function(foil, ...) {
   eqscplot(foil$xu[, 1], foil$zu[, 1], type = 'l', bty = 'n', xlab = "x", ylab = "z", ...)
   lines(foil$xl, foil$zl, ...)
   lines(foil$x, foil$zc, col = '#FF1300', ...)
   lines(foil$x, foil$zt, col = '#34AADC', ...)
+}
+
+flowPlot <- function(x, z, u, w, New = TRUE) {
+  if(New == TRUE) {
+    plot.new()
+    plot.window(xlim = c(min(x), max(x)), ylim = c(min(z), max(z)))
+  }
+  if(sum(is.nan(u)) > 0) u[which(is.nan(u))] <- 0
+  if(sum(is.nan(w)) > 0) w[which(is.nan(w))] <- 0
+  quiver(x, z, u, w)
+}
+
+pressPlot <- function(x, z, u, w, ...) {
+  if(sum(is.nan(u)) > 0) u[which(is.nan(u))] <- 0
+  if(sum(is.nan(w)) > 0) w[which(is.nan(w))] <- 0
+  P <- sqrt((u ^ 2) + (w ^ 2))
+  contour(x = x, y = z, z = P)
+}
+
+thinDisc <- function(X, Z, N) {
+  # Discretize the thin foil into N panels
+  discFoil <- approx(X, Z, n = N + 1)
+  dx <- diff(discFoil$x)
+  dz <- diff(discFoil$y)
+  panelLength <- sqrt(dx ^ 2 + dz ^ 2)
+  
+  # Determine the angle and length of each panel
+  theta <- asin(dz / panelLength)
+  pct <- panelLength * cos(theta)
+  pst <- panelLength * sin(theta)
+  
+  # Set the quarter chord and three-quarter chord points
+  x <- 0.25 * pct + discFoil$x[1:N]
+  z <- 0.25 * pst + discFoil$y[1:N]
+  xc <- 0.75 * pct + discFoil$x[1:N]
+  zc <- 0.75 * pst + discFoil$y[1:N]
+  
+  # Compute the normal vectors
+  nx <- - sin(theta)
+  nz <- cos(theta)
+  
+  return(list(x = x, z = z, xc = xc, zc = zc, nx = nx, nz = nz))
+}
+
+thinPlot <- function(X, Z, foil) {
+  eqscplot(X, Z, type = 'l', bty = "n",
+           col = "#DBDDDE", lwd = 2)
+  points(foil$x, foil$z, pch = 19, col = "#FF3B30")
+  points(foil$xc, foil$zc, pch = 19, col = "#34AADC")
+  arrows(foil$xc, foil$zc, foil$xc + foil$nx, foil$zc + foil$nz,
+         col = "#34AADC")
 }
--- vor2D.cpp
+++ vor2D.cpp
@@ -5,7 +5,7 @@ using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export]]
-arma::vec vor2D (float G, double x, double z, double xj, double zj) {
+arma::rowvec vor2D (double G, double x, double z, double xj, double zj) {
   
   double xmxj = x - xj;
   double zmzj = z - zj;
@@ -14,13 +14,83 @@ arma::vec vor2D (float G, double x, double z, double xj, double zj) {
   double u = premult * zmzj;
   double w = premult * (-1) * xmxj;
   
-  arma::vec ret;
+  arma::rowvec ret;
   ret << u << w;
   return(ret);
 }
 
 // [[Rcpp::export]]
-arma::vec wrapper (float G, double x, double z, double xj, double zj) {
-  arma::vec y = vor2D(G, x, z, xj, zj);
-  return(y);
+List lumpVortex (arma::vec xc, arma::vec zc,
+                 arma::vec x, arma::vec z,
+                 arma::vec nx, arma::vec nz,
+                 float Q, float alpha) {
+  
+  // Initialize output and intermediate matricies
+  int matDim = x.n_elem;
+  arma::mat u(matDim, matDim);
+  arma::mat w(matDim, matDim);
+  arma::mat a(matDim, matDim);
+  
+  // Initialize intermediate vectors
+  arma::rowvec vorOut(2);
+  arma::colvec N(2);
+  arma::colvec RHS(matDim);
+  arma::vec freeStream(2);
+  arma::colvec G(matDim);
+  
+  // Decompose the free stream velocity
+  freeStream << - Q * cos(alpha) << - Q * sin(alpha);
+  
+  // Initialize iterators
+  int i;
+  int j;
+  
+  // Compute the influence coefficients and the RHS
+  for(i = 0; i < matDim; i++) {
+    N << nx(i) << nz(i);
+    RHS(i) = dot(freeStream, N);
+    for(j = 0; j < matDim; j++) {
+      vorOut = vor2D(1, xc(i), zc(i), x(j), z(j));
+      a(i, j) = dot(vorOut, N);
+    }
+  }
+  
+  // Prepare the output
+  List ret;
+  ret["a"] = a;
+  ret["RHS"] = RHS;
+  ret["G"] = solve(a, RHS); // Solve for the vortex strengths
+  return(ret);
+}
+
+// [[Rcpp::export]]
+arma::mat vor2DV (double G, arma::vec x, arma::vec z, arma::vec xj, arma::vec zj) {
+  
+  arma::vec xmxj = x - xj;
+  arma::vec zmzj = z - zj;
+  arma::vec rj2 = pow(xmxj, 2) + pow(zmzj, 2);
+  arma::vec premult = G / 2. / datum::pi / rj2;
+  arma::vec u = premult % zmzj;
+  arma::vec w = premult * (-1) % xmxj;
+  
+  arma::mat ret(x.n_elem, 2);
+  ret.col(0) = u;
+  ret.col(1) = w;
+  return(ret);
+}
+
+// [[Rcpp::export]]
+List vor2DM (double G, arma::mat x, arma::mat z, double xj, double zj) {
+  
+  arma::mat xmxj = x - xj;
+  arma::mat zmzj = z - zj;
+  arma::mat rj2 = pow(xmxj, 2) + pow(zmzj, 2);
+  arma::mat premult = G / 2. / datum::pi / rj2;
+  arma::mat u = premult % zmzj;
+  arma::mat w = premult * (-1) % xmxj;
+  
+  List ret;
+  ret["u"] = u;
+  ret["w"] = w;
+  return(ret);
 }
